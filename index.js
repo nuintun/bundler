78743e18e680ce6643eef30fff9d99d9a651293d
nuintun <nuintun@gmail.com>
2018-01-26 17:34:54 +0800

    :art: Update files

--- index.js
+++ index.js
@@ -4,17 +4,22 @@
  * @version 2018/01/25
  */
 
-import Node from './lib/node';
 import Visitor from './lib/Visitor';
 
 export default class Bundler {
-  constructor(entry, parser, options) {
-    return this.bundle(entry, parser, options);
+  constructor(options) {
+    return this.bundle(options);
   }
 
-  async bundle(entry, parser, options) {
-    return new Promise((resolve, reject)=>{
-      resolve(await new Visitor(new Node(await parser(entry))));
+  bundle(options) {
+    return new Promise(async (resolve, reject) => {
+      const visitor = new Visitor(options);
+
+      try {
+        resolve(await visitor.traverse());
+      } catch (error) {
+        reject(error);
+      }
     });
   }
 }
