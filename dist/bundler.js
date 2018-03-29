03a06475753a51aa52f9d5301f75ca2ce8416c69
nuintun <nuintun@qq.com>
2018-03-29 09:13:41 +0800

    :art: Update version

--- dist/bundler.js
+++ dist/bundler.js
@@ -2,7 +2,7 @@
  * @module bundler
  * @author nuintun
  * @license MIT
- * @version 0.2.0
+ * @version 0.2.2
  * @description A async file dependency bundle parser.
  * @see https://github.com/nuintun/bundler#readme
  */
