577b2d0403bd11cea7e3c1069650dcfaa8b4a84f
nuintun <nuintun@qq.com>
2019-02-27 10:52:22 +0800

    :arrow_up: Bump version to 1.0.0

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
  * @module bundler
  * @author nuintun
  * @license MIT
- * @version 0.3.9
+ * @version 1.0.0
  * @description An async file dependency bundle parser.
  * @see https://github.com/nuintun/bundler#readme
  */
