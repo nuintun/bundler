afb992f080fdae975100954dbd3f1f2456aba1e1
nuintun <nuintun@qq.com>
2019-03-01 15:55:18 +0800

    :arrow_up: Bump version

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
  * @module bundler
  * @author nuintun
  * @license MIT
- * @version 1.0.0
+ * @version 1.0.1
  * @description An async file dependency bundle parser.
  * @see https://github.com/nuintun/bundler#readme
  */
