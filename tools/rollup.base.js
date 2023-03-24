63af71b325b90f861d1e35088915d8fe7adc3c84
nuintun <nuintun@qq.com>
2023-03-24 17:43:38 +0800

    feat(deps): Bump deps

--- tools/rollup.base.js
+++ tools/rollup.base.js
@@ -2,9 +2,11 @@
  * @module rollup.base
  */
 
-import pkg from '../package.json';
-import treeShake from './plugins/tree-shake';
+import { createRequire } from 'module';
+import treeShake from './plugins/tree-shake.js';
 import typescript from '@rollup/plugin-typescript';
+
+const pkg = createRequire(import.meta.url)('../package.json');
 
 const banner = `/**
   * @package ${pkg.name}
@@ -16,22 +18,32 @@ const banner = `/**
   */
  `;
 
+/**
+ * @function rollup
+ * @param esnext
+ * @return {import('rollup').RollupOptions}
+ */
 export default function rollup(esnext) {
   return {
     input: 'src/index.ts',
     output: {
       banner,
-      interop: false,
-      exports: 'auto',
       esModule: false,
-      preferConst: true,
+      exports: 'auto',
+      interop: 'auto',
+      preserveModules: true,
       dir: esnext ? 'esm' : 'cjs',
       format: esnext ? 'esm' : 'cjs',
-      entryFileNames: `[name].${esnext ? 'js' : 'cjs'}`,
-      chunkFileNames: `[name].${esnext ? 'js' : 'cjs'}`
+      generatedCode: { constBindings: true },
+      chunkFileNames: `[name].${esnext ? 'js' : 'cjs'}`,
+      entryFileNames: `[name].${esnext ? 'js' : 'cjs'}`
     },
-    external: ['tslib'],
-    preserveModules: true,
-    plugins: [typescript(), treeShake()]
+    plugins: [typescript(), treeShake()],
+    onwarn(error, warn) {
+      if (error.code !== 'CIRCULAR_DEPENDENCY') {
+        warn(error);
+      }
+    },
+    external: ['tslib']
   };
 }
